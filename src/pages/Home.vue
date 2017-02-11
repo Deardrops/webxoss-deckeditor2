2892098b239f13a10cd617503602b466bcfb1c93
webxoss <webxoss@gmail.com>
2017-02-11 18:16:57 +0800

    add introduce to home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,16 @@
 <script>
+import marked from 'marked'
 import { AppHeader, HeaderIcon } from 'components/AppHeader'
 export default {
   components: {
     AppHeader,
     HeaderIcon,
+  },
+  data: () => ({
+    introduce: require('./introduce.md'),
+  }),
+  methods: {
+    marked,
   },
 }
 </script>
@@ -14,6 +21,38 @@ export default {
       <header-icon slot="left" name="menu"/>
       <header-icon slot="right" name="more"/>
     </app-header>
-    <router-link to="/deck">Deck Editor</router-link>
+    <article>
+      <section v-html="marked(introduce)" :class="$style.markdown"></section>
+    </article>
   </div>
 </template>
+
+<style module>
+@import "css/vars.css";
+.markdown {
+  padding: 0 1em;
+  line-height: 1.5;
+  font-size: 1.2em;
+
+  & p,
+  & h1,
+  & h2,
+  & ul {
+    margin: 1em 0;
+  }
+
+  & h1 {
+    font-size: 120%;
+    font-weight: bold;
+    text-align: center;
+  }
+
+  & h2 {
+    font-size: 110%;
+  }
+
+  & a {
+    color: var(--main-color);
+  }
+}
+</style>
---
+++ src/pages/introduce.md
@@ -0,0 +1,9 @@
+# WEBXOSS | NEXT
+
+*This project is under developing.*
+
+* Twitter: [@WEBXOSS](https://twitter.com/WEBXOSS)
+* E-mail: [webxoss@gmail.com](mailto:webxoss@gmail.com)
+* GitHub: [webxoss-deckeditor2](https://github.com/Deardrops/webxoss-deckeditor2)
+
+## [👉 Try our new DeckEditor](#/deck)
