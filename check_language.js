b52eb2009033213f614aa6008ec5767c275fef00
deardrops <lness@qq.com>
2017-02-22 23:30:30 +0800

    var -> const
    now jslint passed

--- check_language.js
+++ check_language.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const rf = require("fs")
+const rf = require('fs')
 
 const lang_dir = './src/lang/'
 
@@ -15,36 +15,36 @@ const lang_names = [
   'zh_Hans',
 ]
 
-var lang_maps = {}
+const lang_maps = {}
 
 lang_names.forEach(function (name) {
-  var hjson = rf.readFileSync(lang_dir + name + '.hjson', 'utf-8')
-  var json = Hjson.parse(hjson)
+  const hjson = rf.readFileSync(lang_dir + name + '.hjson', 'utf-8')
+  const json = Hjson.parse(hjson)
   lang_maps[name] = json
 })
 
 const _ = require('lodash')
 
 function check_keys(name) {
-  var keys = Object.keys(lang_maps[name])
-  var origin_keys = Object.keys(lang_maps['en'])
-  var surplus_keys = _.difference(keys, origin_keys)
+  const keys = Object.keys(lang_maps[name])
+  const origin_keys = Object.keys(lang_maps['en'])
+  const surplus_keys = _.difference(keys, origin_keys)
   if (surplus_keys.length !== 0) {
     console.log('Surplus keys in ' + name)
     console.log(surplus_keys)
   }
-  var lack_keys = _.difference(origin_keys, keys)
+  const lack_keys = _.difference(origin_keys, keys)
   if (lack_keys.length !== 0) {
     console.log('Lack keys in ' + name)
     console.log(lack_keys)
   }
 }
 
-for (var name in lang_maps) {
+lang_names.forEach(function (name) {
   if (name === 'en') {
-    continue
+    return
   }
   check_keys(name)
-}
+})
 
 console.log('All Done!')
