544f8bdeb2c90b1b4d67ed06cc73e96689bd4c99
deardrops <lness@qq.com>
2017-02-23 22:53:43 +0800

    es6 code style
    use string template, =>
    rename checkFail -> checkFailed
    move require to top

--- check_language.js
+++ check_language.js
@@ -1,58 +1,53 @@
 'use strict'
 
 const fs = require('fs')
+const _ = require('lodash')
+const Hjson = require('hjson')
 
 const dir = './src/lang/'
-
 let names = []
-
 if (!fs.statSync(dir).isDirectory()) {
-  console.error('no such directory: ' + dir)
+  console.error(`no such directory: ${dir}`)
   process.exit(1)
 } else {
   names = fs.readdirSync(dir)
 }
 
-const Hjson = require('hjson')
-
 let maps = {}
-
-names.forEach(function (name) {
-  let hjson = fs.readFileSync(dir + name, 'utf-8')
+names.forEach(name => {
+  let hjson = fs.readFileSync(`${dir}${name}`, 'utf-8')
   let json = Hjson.parse(hjson)
   maps[name] = json
 })
 
-const _ = require('lodash')
-
-let checkFail = false
-
+let checkFailed = false
 function checkLanguage(name) {
   let keys = Object.keys(maps[name])
   let originKeys = Object.keys(maps['en.hjson'])
   let surplusKeys = _.difference(keys, originKeys)
   if (surplusKeys.length) {
-    console.warn('Surplus keys in ' + name)
+    console.warn(`Surplus keys in ${name}`)
     console.log(surplusKeys)
-    checkFail = true
+    checkFailed = true
   }
   let lackKeys = _.difference(originKeys, keys)
   if (lackKeys.length) {
-    console.warn('Lack keys in ' + name)
+    console.warn(`Lack keys in ${name}`)
     console.log(lackKeys)
-    checkFail = true
+    checkFailed = true
   }
 }
 
-names.forEach(function (name) {
+names.forEach(name => {
   if (name === 'en.hjson') {
     return
   }
   checkLanguage(name)
 })
 
-if (checkFail) {
+if (checkFailed) {
   process.exit(1)
 } else {
-  console.log('All Done!')
+  console.log(names)
+  console.log('All language file test passed!')
 }
