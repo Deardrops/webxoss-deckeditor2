58540860d56560c0870af8834897bf5a8f07cc3f
deardrops <lness@qq.com>
2017-02-25 17:11:14 +0800

    specify test by 'npm run test xxx'

--- test/index.js
+++ test/index.js
@@ -1,3 +1,20 @@
 'use strict'
-require('./language.js')
-require('./complie.js')
+const tests = {
+  'language': './language.js',
+  'complie': './complie.js',
+}
+
+const args = process.argv.slice(2)
+if (!args.length) {
+  // default: run all tests
+  for (let name in tests) {
+    require(tests[name])
+  }
+} else {
+  let argv = args[0]
+  if (argv in tests) {
+    require(tests[argv])
+  } else {
+    console.warn(`No such test: ${argv}`)
+  }
+}
