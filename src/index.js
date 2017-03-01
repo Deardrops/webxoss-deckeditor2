bcc8b0b2ec46824bd7caad6089de17f92125e58c
deardrops <lness@qq.com>
2017-03-01 17:21:07 +0800

    add ko / ru CardInfo

--- src/index.js
+++ src/index.js
@@ -10,20 +10,53 @@ import 'js/global'
 
 import ImageFileCache from 'js/ImageFileCache'
 
-import { $get } from 'js/util'
+// import { $get } from 'js/util'
 
 sync(store, router)
 
+// function addToCardInfo(CardInfo, suffix) {
+//   if (!suffix) {
+//     return
+//   }
+
+//   $get(`./lang/${suffix}.json`)
+//     .then(newCardInfo => {
+//       for (let pid in CardInfo) {
+//         let info = CardInfo[pid]
+//         let newInfo = newCardInfo[pid] || {}
+//         for (let prop in info) {
+//           let idx = prop.indexOf('_en')
+//           if (idx === -1) {
+//             continue
+//           }
+//           let newProp = prop.slice(0, idx) + suffix
+//           info[newProp] = newInfo[newProp] || info[prop]
+//         }
+//       }
+//     })
+// }
+
 // Deardrops: Add error meassage if load json fail
-$get('./CardInfo.json')
-  .then(CardInfo => {
-    window.CardInfo = Object.freeze(CardInfo)
-    ImageFileCache.init(() => {
-      new Vue({
-        el: '#app',
-        router,
-        store,
-        ...App,
-      })
-    }, 1000)
+// $get('./CardInfo.json')
+//   .then(CardInfo => {
+//     // addToCardInfo(CardInfo, '_ko')
+//     // addToCardInfo(CardInfo, '_ru')
+//     window.CardInfo = Object.freeze(CardInfo)
+//     ImageFileCache.init(() => {
+//       new Vue({
+//         el: '#app',
+//         router,
+//         store,
+//         ...App,
+//       })
+//     }, 1000)
+//   })
+
+ImageFileCache.init(() => {
+  new Vue({
+    el: '#app',
+    router,
+    store,
+    ...App,
   })
+}, 1000)
