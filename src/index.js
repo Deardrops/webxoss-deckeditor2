7eabdd57bd63ece5f4813dc717d3667fb44533f9
webxoss <webxoss@gmail.com>
2017-03-04 21:11:03 +0800

    import CardInfo files from webxoss.com

--- src/index.js
+++ src/index.js
@@ -10,25 +10,40 @@ import 'js/global'
 
 import ImageFileCache from 'js/ImageFileCache'
 
-import { $get } from 'js/util'
+// import { $get } from 'js/util'
 
 sync(store, router)
 
 // Deardrops: Add error meassage if load json fail
-$get('./CardInfo.json')
-  .then(CardInfo => {
-    window.CardInfo = Object.freeze(CardInfo)
-    ImageFileCache.init()
-    .catch(error => {
-      console.error('Error in ImageFileCache.init():')
-      console.error(error)
-    })
-    .then(() => {
-      new Vue({
-        el: '#app',
-        router,
-        store,
-        ...App,
-      })
-    })
+// $get('./CardInfo.json')
+//   .then(CardInfo => {
+//     window.CardInfo = Object.freeze(CardInfo)
+//     ImageFileCache.init()
+//     .catch(error => {
+//       console.error('Error in ImageFileCache.init():')
+//       console.error(error)
+//     })
+//     .then(() => {
+//       new Vue({
+//         el: '#app',
+//         router,
+//         store,
+//         ...App,
+//       })
+//     })
+//   })
+
+
+ImageFileCache.init()
+.catch(error => {
+  window.alert('Error in ImageFileCache.init()')
+  console.error(error)
+})
+.then(() => {
+  new Vue({
+    el: '#app',
+    router,
+    store,
+    ...App,
   })
+})
